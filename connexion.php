102464c44caf68fcafaef678882008ba68e523d0
younesskhamlichi <115074765+younesskhamlichi@users.noreply.github.com>
2023-03-20 15:47:23 +0100

    Add files via upload

---
+++ connexion.php
@@ -0,0 +1,8 @@
+<?php
+            $server = 'localhost';
+			$username = 'root';
+			$password = '';
+			$basededonné = 'gestion_des_emprunts';
+			
+			$connexion = new mysqli ($server, $username, $password, $basededonné );
+?>
